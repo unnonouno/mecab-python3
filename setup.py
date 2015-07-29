ac29a033d61bf669d7e9312f57c9930b30c5c983
Yuya Unno <unnonouno@gmail.com>
2015-07-29 07:58:00 +0000

    Use subprocess for readable error message

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from distutils.core import setup,Extension,os
 import string
 import os
 import sys
+import subprocess
 
 def read_file(filename):
     filepath = os.path.join(
@@ -15,13 +16,13 @@ def read_file(filename):
 
 if sys.version > '3':
     def cmd1(strings):
-        return os.popen(strings).readlines()[0][:-1]
+        return subprocess.check_output(strings).strip()
 
     def cmd2(strings):
         return cmd1(strings).split()
 else:
     def cmd1(strings):
-        return os.popen(strings).readlines()[0][:-1]
+        return subprocess.check_output(strings).strip()
 
     def cmd2(strings):
         return string.split(cmd1(strings))
@@ -38,9 +39,9 @@ setup(name = "mecab-python3",
     ext_modules = [
         Extension("_MeCab",
         ["MeCab_wrap.cxx",],
-        include_dirs=cmd2("mecab-config --inc-dir"),
-        library_dirs=cmd2("mecab-config --libs-only-L"),
-        libraries=cmd2("mecab-config --libs-only-l"))
+        include_dirs=cmd2(["mecab-config", "--inc-dir"]),
+        library_dirs=cmd2(["mecab-config", "--libs-only-L"]),
+        libraries=cmd2(["mecab-config", "--libs-only-l"]))
     ],
     classifiers = [
         "Programming Language :: Python",
